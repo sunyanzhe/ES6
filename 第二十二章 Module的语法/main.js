ba528fa4c188757ec470b0ec0980ae2b7c756f0d
sunyz <sunyz@lenovocloud.com>
2019-02-23 20:54:44 +0800

    22.5 模块的整体加载

--- 第二十二章 Module的语法/main.js
+++ 第二十二章 Module的语法/main.js
@@ -1,4 +1,9 @@
 import {firstName, lastName, year} from './profile'
+import {area, circumference} from './circle'
+
 function setName(element) {
     element.textContent = `${firstName} ${lastName}`
 }
+
+console.log('圆面积: ' + area(4));
+console.log('圆周长: ' + circumference(14));
